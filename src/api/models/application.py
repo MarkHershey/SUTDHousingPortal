41e562921d7624491f8e743ba4a703bd9b2460ec
Mark Huang <hhzeos@gmail.com>
2021-03-05 12:18:18 +0800

    Update data models

--- src/api/models/application.py
+++ src/api/models/application.py
@@ -2,13 +2,31 @@ from typing import Dict, List, Optional
 
 from markkk.logger import logger
 from pydantic import BaseModel, validator
+from .room import RoomProfile
+from datetime import datetime, date
+from .lifestyle import LifestyleProfile
+
+
+class TimePeriod(BaseModel):
+    start_date: date
+    end_date: date
 
 
 class ApplicationForm(BaseModel):
     uid: str = None
-    pass
+    created_at: datetime = None
+    student_id: str
+    room_profile: RoomProfile
+    lifestyle_profile: LifestyleProfile
+    stay_period: TimePeriod
 
 
 class ApplicationPeriod(BaseModel):
     uid: str = None
-    pass
+    created_at: datetime = None
+    created_by: str  # username
+    application_window_open: datetime
+    application_window_close: datetime
+    applicable_periods: List[TimePeriod]
+    applicable_rooms: List[str]  # list of room IDs
+    applicable_students: List[str]  # list of student IDs
