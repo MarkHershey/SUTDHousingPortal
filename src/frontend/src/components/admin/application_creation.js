e2ec66a4079cf20f9190837e63957a2fff10d580
Fattyboy9898 <justin_peng@mymail.sutd.edu.sg>
2021-04-06 01:07:56 +0800

    application_creation page for admin is able to create application periods

--- src/frontend/src/components/admin/application_creation.js
+++ src/frontend/src/components/admin/application_creation.js
@@ -2,6 +2,7 @@ import * as bs from "react-bootstrap";
 import React from "react";
 import styled from "styled-components";
 import submitApplicationPeriod from "../../variables/applicationperiodinfo";
+import { InputLabel, MenuItem, Select} from '@material-ui/core'
 
 const Field = styled.p`
   color: #3C64B1;
@@ -25,6 +26,13 @@ const EventDiv = styled.div`
   text-align: center;
 `;
 
+const Apply2BtnSet = styled.div`
+  background-color: #F3F6FA;
+  margin: 20pt 0;
+  padding: 20pt 20pt;
+  border-radius: 20pt;
+`;
+
 export default class ApplicationCreation extends React.Component{
     constructor(props){
         super(props);
@@ -34,10 +42,11 @@ export default class ApplicationCreation extends React.Component{
         this.createUI = this.createUI.bind(this);
         this.addClick = this.addClick.bind(this);
         this.removeClick = this.removeClick.bind(this);
+        this.handleDropdown = this.handleDropdown.bind(this);
         this.state = {
             application_window_open: "",
             application_window_close: "",
-            applicable_periods: [{start_date:"",end_date:""}],
+            applicable_periods: [{"start_date":"","end_date":""}],
             applicable_rooms: [],
             applicable_students: [{firstName:"",lastName:""}],
         };
@@ -47,13 +56,15 @@ export default class ApplicationCreation extends React.Component{
         console.log("new event submit");
         if (this.state.application_window_open === "" ||
             this.state.application_window_close === "" ||
-            this.state.applicable_periods === "" ||
-            this.state.applicable_rooms === "" ||
-            this.state.applicable_students === "") {
+            this.state.applicable_periods === "" 
+            //this.state.applicable_rooms === "" ||
+            //this.state.applicable_students === ""
+            ) {
             alert("Invalid Form!")
             return;
         }
-        submitApplicationPeriod(this.state);
+        submitApplicationPeriod(this.state.application_window_open,this.state.application_window_close,
+            this.state.applicable_periods,this.state.applicable_rooms,this.state.applicable_students);
     }
 
     handleChange(event) {
@@ -74,23 +85,27 @@ export default class ApplicationCreation extends React.Component{
      }
 
     createUI(){
-        console.log("creating");
         return this.state.applicable_periods.map((el, i) => 
             <EventDiv key={i}>
                 <bs.Container>
                     <bs.Row>
                         <bs.Col lg={3}><Field>Start Date:</Field></bs.Col>
                         <bs.Col lg={3}>
-                            <input type="datetime-local" name="firstName" value={el.start_date ||''} onChange={this.handleChangeArrays.bind(this, i)} />
-                        </bs.Col>
-                        <bs.Col lg={3}><Field>End Date:</Field>
-                            <input type="datetime-local" name="lastName" value={el.end_date ||''} onChange={this.handleChangeArrays.bind(this, i)} />
+                            <input type="date" name="start_date" value={el.start_date ||''} onChange={this.handleChangeArrays.bind(this, i)} />
                         </bs.Col>
                     </bs.Row>
                     <bs.Row>
-                        <input type='button' value='remove' onClick={this.removeClick.bind(this, i)}/>
-                        <input type='button' value='add more' onClick={this.addClick.bind(this)}/>
-                        <input type="submit" value="Submit" />
+                        <bs.Col lg={3}><Field>End Date:</Field></bs.Col>
+                        <bs.Col lg={3}>
+                            <input type="date" name="end_date" value={el.end_date ||''} onChange={this.handleChangeArrays.bind(this, i)} />
+                        </bs.Col>
+                    </bs.Row>
+                    <bs.Row>
+                        <bs.Col lg={3}></bs.Col>
+                        <bs.Col lg={3}>
+                            <input type='button' value='remove' onClick={this.removeClick.bind(this, i)}/>
+                            <input type='button' value='add more' onClick={this.addClick.bind(this)}/>    
+                        </bs.Col>   
                     </bs.Row>                    
                 </bs.Container>
             </EventDiv>          
@@ -109,6 +124,12 @@ export default class ApplicationCreation extends React.Component{
         this.setState({ applicable_periods });
     }
 
+    handleDropdown(event){
+        const name = event.target.name;
+        this.state.title = name;
+        console.log(this.state);
+    }
+
     render() {
         return (
             <EventDiv>
@@ -124,20 +145,32 @@ export default class ApplicationCreation extends React.Component{
                         <bs.Row>
                             <bs.Col lg={3}><Field>Applicable periods:</Field></bs.Col>
                         </bs.Row>
-                        <bs.Row>
-                            <form onSubmit={this.handleSubmit}>
-                                {this.createUI()}        
-                            </form>
-                        </bs.Row>
-                                
+                            {this.createUI()}
                         <bs.Row>
                             <bs.Col lg={3}><Field>Applicable Rooms:</Field></bs.Col>
                             <bs.Col lg={3}><input name="applicable_rooms" type="text" onChange={e => this.handleChange(e)}/></bs.Col>
+                        </bs.Row>
+                        <bs.Row>
                             <bs.Col lg={3}><Field>Applicable students:</Field></bs.Col>
-                            <bs.Col lg={3}><Field></Field></bs.Col>
+                            <bs.Col lg={3}>
+                                <select name="applicable_students" id="applicable_students">
+                                    <option value="Freshmore">Freshmore</option>
+                                    <option value="Undergraduate">Undergraduate</option>
+                                    <option value="Master's">Master's</option>
+                                    <option value="PhD">PhD</option>
+                                </select>
+                            </bs.Col>
                         </bs.Row>
                     </bs.Container>
                 </EditBox>
+                <br/>
+                <Apply2BtnSet>
+                    <bs.Container>
+                        <bs.Row> 
+                            <bs.Col><button type="button" className="btn btn-outline-primary" onClick={this.handleSubmit}>Create Application Period</button></bs.Col>
+                        </bs.Row>
+                    </bs.Container>
+                </Apply2BtnSet>
             </EventDiv>
                 );
     };
--- src/frontend/src/variables/applicationperiodinfo.js
+++ src/frontend/src/variables/applicationperiodinfo.js
@@ -13,25 +13,25 @@ var applicable_students = []// list of student ids
 var application_forms = [] // list of application form objects
 
 
-export default async function submitApplicationPeriod(json){
-    var data = JSON.stringify({"uid":"string","created_at":"2021-04-05T06:18:04.446Z",
-    "created_by":"string","application_window_open":"2021-04-05T06:18:04.446Z",
-    "application_window_close":"2021-04-05T06:18:04.446Z",
-    "applicable_periods":[{"start_date":"2021-04-05","end_date":"2021-04-05"}],
-    "applicable_rooms":["string"],"applicable_students":["string"],
-    "application_forms_map":{"additionalProp1":"string","additionalProp2":"string",
-    "additionalProp3":"string"},"reference_count":0});
-
+export default async function submitApplicationPeriod(application_window_open,application_window_close,
+    applicable_periods,applicable_rooms,applicable_students){
+    var data = JSON.stringify({"application_window_open":application_window_open,
+    "application_window_close":application_window_close,
+    "applicable_periods":[{"start_date":"2021-04-10","end_date":"2021-04-11"}],
+    "applicable_rooms":["string"],"applicable_students":["string"]
+    });
     var config = {
     method: 'post',
     url: url+'/api/application_periods/',
     headers: { 
         'accept': 'application/json', 
+        'Authorization': 'Bearer '+getToken(), 
         'Content-Type': 'application/json'
     },
     data : data
+    
     };
-
+    console.log(data);
     axios(config)
     .then(function (response) {
     console.log(JSON.stringify(response.data));
