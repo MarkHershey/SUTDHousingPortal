1966454d057b3b7a6f2b2a9ed64a52f9c2eefb72
Fattyboy9898 <justin_peng@mymail.sutd.edu.sg>
2021-04-07 16:43:27 +0800

    added id values for profile room and personal

--- src/frontend/src/components/personal_profile_edit.js
+++ src/frontend/src/components/personal_profile_edit.js
@@ -69,22 +69,22 @@ export default class PersonalDataEdit extends React.Component{
                     <bs.Container>
                         <bs.Row>
                             <bs.Col><Field>Personal Email:</Field></bs.Col>
-                            <bs.Col><input name="email_personal" type="email" placeholder={this.state.email_personal} onChange={e => this.handleChange(e)}/></bs.Col>
+                            <bs.Col><input id = "ppl_prof_email" name="email_personal" type="email" placeholder={this.state.email_personal} onChange={e => this.handleChange(e)}/></bs.Col>
                             <bs.Col><Field>Preferred Roommate</Field></bs.Col>
                             <bs.Col><input id = "ppl_prof_roommate" name="preference_roommate" type="text" placeholder={this.state.preference_roommate}/></bs.Col>
                         </bs.Row>
     
                         <bs.Row>
                             <bs.Col><Field>Phone Number:</Field></bs.Col>
-                            <bs.Col><input name="phone_number"type="number" placeholder={this.state.phone_number} onChange={e => this.handleChange(e)}/></bs.Col>
+                            <bs.Col><input id="ppl_prof_phone_number" name="ppl_prof_phone_number" type="number" placeholder={this.state.phone_number} onChange={e => this.handleChange(e)}/></bs.Col>
                             <bs.Col><Field>Local Address Street</Field></bs.Col>
                             <bs.Col><input id = "ppl_prof_local_addr_street" name="local_addr_street" type="text" placeholder={this.state.local_addr_street} onChange={e => this.handleChange(e)}/></bs.Col>
                         </bs.Row>
                         <bs.Row>
                             <bs.Col><Field>Local Address Unit Number:</Field></bs.Col>
-                            <bs.Col><input name="local_addr_unit" type="text" placeholder={this.state.local_addr_unit} onChange={e => this.handleChange(e)}/></bs.Col>
+                            <bs.Col><input id="ppl_prof_local_addr_unit" name="local_addr_unit" type="text" placeholder={this.state.local_addr_unit} onChange={e => this.handleChange(e)}/></bs.Col>
                             <bs.Col><Field>Local Address Postal Code:</Field></bs.Col>
-                            <bs.Col><input name="local_addr_post_code" type="text" placeholder={this.state.local_addr_post_code} onChange={e => this.handleChange(e)}/></bs.Col>
+                            <bs.Col><input id="ppl_prof_local_addr_post_code" name="local_addr_post_code" type="text" placeholder={this.state.local_addr_post_code} onChange={e => this.handleChange(e)}/></bs.Col>
                         </bs.Row>
                         
                     </bs.Container>
