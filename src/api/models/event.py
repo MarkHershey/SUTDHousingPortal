8d3390cc3d38e75b4d6e7f6babc599f54344a4eb
Mark Huang <hhzeos@gmail.com>
2021-04-21 03:58:34 +0800

    Update Event model and unittest

--- src/api/models/event.py
+++ src/api/models/event.py
@@ -14,8 +14,8 @@ class Event(BaseModel):
     title: str
     event_type: str  # example: FE, IBE, etc
     meetup_location: str
-    block: str = "Any"
-    floor: str = "Any"
+    block: str = "ANY"
+    floor: str = "ANY"
     description: str = "None"  # long text description of the event
     start_time: datetime  # datetime object indicating event start time
     duration_mins: int = 60  # event duration in number of minutes
@@ -36,13 +36,70 @@ class Event(BaseModel):
     def default_created_at(cls, v):
         return v or datetime.now()
 
+    @validator("event_type", pre=True, always=True)
+    def validate_event_type(cls, v):
+        if isinstance(v, str) and v.upper() in ("IBE", "FE", "BE"):
+            return v.upper()
+        else:
+            return "MEETUP"
+
+    @validator("block", pre=True, always=True)
+    def validate_block(cls, v):
+        if isinstance(v, str) and v.upper() in ("59", "57", "55", "ANY"):
+            return v.upper()
+        else:
+            return "ANY"
+
+    @validator("floor", pre=True, always=True)
+    def validate_floor(cls, v):
+        if isinstance(v, str) and v.upper() != "ANY":
+            try:
+                v = str(int(v))
+            except:
+                pass
+
+        if isinstance(v, str) and v.upper() in (
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "ANY",
+        ):
+            return v.upper()
+        else:
+            return "ANY"
+
+    @validator("duration_mins", pre=True, always=True)
+    def validate_duration_mins(cls, v):
+        if isinstance(v, int) and v > 0:
+            return v
+        else:
+            return 60
+
+    @validator("signup_limit", pre=True, always=True)
+    def validate_signup_limit(cls, v):
+        if isinstance(v, int) and 0 < v <= 300:
+            return v
+        else:
+            return 20
+
     @validator("signup_ddl", pre=True, always=True)
     def default_signup_ddl(cls, v, *, values, **kwargs):
         return v or values["start_time"]
 
     @validator("archived", pre=True, always=True)
     def default_archived(cls, v):
-        return False
+        if isinstance(v, bool):
+            return v or False
+        else:
+            return False
 
 
 class EventEditableInfo(BaseModel):
